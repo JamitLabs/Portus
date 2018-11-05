00c2aa17ae5de4e7aeddef49c68af27a4ad27902
Andreas Link <andreas.link@jamitlabs.com>
2018-11-05 21:30:57 +0100

    Refine routing algorithm

--- Frameworks/Portus/Models/Routable.swift
+++ Frameworks/Portus/Models/Routable.swift
@@ -6,42 +6,52 @@
 import Foundation
 
 public protocol Routable: class {
-    var routingID: RoutingID { get }
+    // MARK: - Properties
+    var routableID: RoutableID { get }
 
-    func route(from origin: Path, to destination: Path)
+    // MARK: - Methods
     func willDismiss(completion: @escaping () -> Void)
     func dismiss(animated: Bool, completion: @escaping () -> Void)
-    func open(_ routable: Routable, animated: Bool, completion: @escaping () -> Void)
+    func open(_ routingID: RoutableID, animated: Bool, completion: @escaping (Routable?) -> Void)
+    func route(from origin: Path, to destination: Path)
 }
 
-extension Routable { // swiftlint:disable:this extension_access_modifier
-    public func route(from origin: Path, to destination: Path) {
-        switch (origin.firstPathComponent, destination.firstPathComponent) {
-        case (_, .none):
-            // Reached the desired location
-            return
+extension Routable {
+    // MARK: - Properties
+    var routableID: RoutableID { return String(describing: self) }
 
-        case let (.none, second?):
-            open(second, animated: false) {
-                second.routable.route(from: origin.remainingPath, to: destination.remainingPath)
+    // MARK: - Methods
+    public func willDismiss(completion: @escaping () -> Void) { completion() }
+
+    public func route(from origin: Path, to destination: Path) {
+        switch (origin.nextPathComponent, destination.nextPathComponent) {
+        case (_, .none):
+            return // NOTE: We are already at the desired location
+
+        case let (.none, secondID?):
+            open(secondID, animated: false) { [weak self] routable in
+                guard let self = self, let routable = routable else { return }
+                RoutingTree.shared.add(routable: routable, parent: self)
+                routable.route(from: origin.remainingPath, to: destination.remainingPath)
             }
 
-        case let (first?, second?):
-            if first == second {
-                second.routable.route(from: origin.remainingPath, to: destination.remainingPath)
+        case let (firstID?, secondID?):
+            guard let currentRoutable = RoutingTree.shared.getRoutableWith(identifier: firstID) else { return }
+
+            if firstID == secondID {
+                currentRoutable.route(from: origin.remainingPath, to: destination.remainingPath)
             } else {
-                first.routable.willDismiss {
-                    first.routable.dismiss(animated: false) { [weak self] in
-                        self?.open(second.routable, animated: false) {
-                            second.routable.route(from: origin.remainingPath, to: destination.remainingPath)
+                currentRoutable.willDismiss {
+                    currentRoutable.dismiss(animated: false) { [weak self] in
+                        guard let self = self else { return }
+                        self.open(secondID, animated: false) { [weak self] routable in
+                            guard let self = self, let routable = routable else { return }
+                            RoutingTree.shared.add(routable: routable, parent: self)
+                            routable.route(from: origin.remainingPath, to: destination.remainingPath)
                         }
                     }
                 }
             }
         }
-    }
-
-    public func willDismiss(completion: @escaping () -> Void) {
-        completion()
     }
 }
