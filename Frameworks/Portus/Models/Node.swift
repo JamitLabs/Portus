7bcf88982f1c9297c17c47c3dfaf47c11e9c869b
Andreas Link <andreas.link@jamitlabs.com>
2018-11-05 22:06:56 +0100

    Refine Node

--- Frameworks/Portus/Models/Node.swift
+++ Frameworks/Portus/Models/Node.swift
@@ -19,15 +19,20 @@ public class Node {
     }
 
     // MARK: - Methods
+    @discardableResult
     func removeSubNode(routableID: RoutableID) -> Bool {
-        guard let indexToRemove = subNodes.firstIndex(where: { $0.routableID == routableID } ) else { return false }
+        guard let indexToRemove = subNodes.firstIndex(where: { $0.routableID == routableID }) else { return false }
         subNodes.remove(at: indexToRemove)
         return true
     }
 
     func searchNodeUsingDFS(routableID: RoutableID) -> Node? {
         guard self.routableID != routableID else { return self }
-        for subNode in subNodes { if let node = subNode.searchNodeUsingDFS(routableID: routableID) { return node } }
+        for subNode in subNodes {
+            guard let node = subNode.searchNodeUsingDFS(routableID: routableID) else { continue }
+            return node
+        }
+
         return nil
     }
 }
