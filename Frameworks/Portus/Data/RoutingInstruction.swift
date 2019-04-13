134a8822af3d0fc2c8ccbe17a7af9db180ee4251
Andreas Link <andreas.link@jamitlabs.com>
2019-04-13 20:00:18 +0200

    Extend RoutingInstruction

--- Frameworks/Portus/Data/RoutingInstruction.swift
+++ Frameworks/Portus/Data/RoutingInstruction.swift
@@ -7,8 +7,21 @@ import Foundation
 
 public typealias RoutingInstructions = [RoutingInstruction]
 
-public enum RoutingInstruction {
-    case leave(entry: RoutingEntry, animated: Bool)
+public enum RoutingInstruction: CustomStringConvertible {
     case enter(entry: RoutingEntry, animated: Bool)
-    case switchTo(entry: RoutingEntry, animated: Bool)
+    case leave(entry: RoutingEntry, animated: Bool)
+    case switchTo(entry: RoutingEntry, from: RoutingEntry, animated: Bool)
+
+    public var description: String {
+        switch self {
+        case let .enter(entry, _):
+            return "enter(\(entry.identifier.rawValue))"
+
+        case let .leave(entry, _):
+            return "leave(\(entry.identifier.rawValue))"
+
+        case let .switchTo(entry, _, _):
+            return "switchTo(\(entry.identifier.rawValue))"
+        }
+    }
 }
