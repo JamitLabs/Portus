ba9518dbfbb279117d6885e25cd51620045763ce
Andreas Link <andreas.link@jamitlabs.com>
2019-04-13 19:56:16 +0200

    Introduce try collection extension

---
+++ Frameworks/Portus/Extensions/CollectionExtension.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Andreas Link on 07.04.19.
+//  Copyright © 2019 Jamit Labs. All rights reserved.
+//
+
+import Foundation
+
+extension Collection {
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    internal subscript (try index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
