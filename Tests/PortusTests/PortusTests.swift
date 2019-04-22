aeeae8c1ff405a4f8da78024d8b6743343f58055
Andreas Link <andreas.link@jamitlabs.com>
2019-04-22 09:45:19 +0200

    Extend unit tests

--- Tests/PortusTests/PortusTests.swift
+++ Tests/PortusTests/PortusTests.swift
@@ -11,10 +11,45 @@ enum RoutingTable {
         static let staticDestination1: StaticRoutingDestination = [.switchNode1, .node2, .switchNode3, .node12].entries
         static let staticDestination2: StaticRoutingDestination = [.switchNode1, .node1, .node5, .node8].entries
         static let staticDestination3: StaticRoutingDestination = [.switchNode1, .switchNode2, .node4].entries
+        static let staticDestination4: StaticRoutingDestination = [.switchNode1, .node1, .node6, .node11].entries
+        static let staticDestination5: StaticRoutingDestination = [.switchNode1, .node2, .switchNode3, .node13].entries
     }
 }
 
 class PortusTests: XCTestCase {
+    private let expectedRoutingInstructions1: RoutingInstructions = [
+        .switchTo(entry: RoutingEntry(identifier: .node2), switchNodeEntry: RoutingEntry(identifier: .switchNode1)),
+        .enter(entry: RoutingEntry(identifier: .switchNode3)),
+        .switchTo(entry: RoutingEntry(identifier: .node12), switchNodeEntry: RoutingEntry(identifier: .switchNode3))
+    ]
+
+    private let expectedRoutingInstructions2: RoutingInstructions = [
+        .leave(entry: RoutingEntry(identifier: .switchNode3)),
+        .switchTo(entry: RoutingEntry(identifier: .node1), switchNodeEntry: RoutingEntry(identifier: .switchNode1)),
+        .enter(entry: RoutingEntry(identifier: .node5)),
+        .enter(entry: RoutingEntry(identifier: .node8))
+    ]
+
+    private let expectedRoutingInstructions3: RoutingInstructions = [
+        .leave(entry: RoutingEntry(identifier: .node8)),
+        .leave(entry: RoutingEntry(identifier: .node5)),
+        .switchTo(entry: RoutingEntry(identifier: .switchNode2), switchNodeEntry: RoutingEntry(identifier: .switchNode1)),
+        .switchTo(entry: RoutingEntry(identifier: .node4), switchNodeEntry: RoutingEntry(identifier: .switchNode2))
+    ]
+
+    private let expectedRoutingInstructions4: RoutingInstructions = [
+        .switchTo(entry: RoutingEntry(identifier: .node1), switchNodeEntry: RoutingEntry(identifier: .switchNode1)),
+        .enter(entry: RoutingEntry(identifier: .node6)),
+        .enter(entry: RoutingEntry(identifier: .node11))
+    ]
+
+    private let expectedRoutingInstructions5: RoutingInstructions = [
+        .leave(entry: RoutingEntry(identifier: .node11)),
+        .leave(entry: RoutingEntry(identifier: .node6)),
+        .switchTo(entry: RoutingEntry(identifier: .node2), switchNodeEntry: RoutingEntry(identifier: .switchNode1)),
+        .enter(entry: RoutingEntry(identifier: .switchNode3))
+    ]
+
     override static func setUp() {
         super.setUp()
 
@@ -27,93 +62,66 @@ class PortusTests: XCTestCase {
         RoutingTree.default.root = nil
     }
 
-    // swiftlint:disable:next function_body_length
     func testRouteToStaticDestinations() {
-        RoutingTree.default.root = nil
-
         let root = SwitchNode1()
         root.startIfNeeded()
-        let exp1 = expectation(description: "Routed successfully to destination 1")
-        let exp2 = expectation(description: "Routed successfully to destination 2")
-        let exp3 = expectation(description: "Routed successfully to destination 3")
+        let exp = expectation(description: "Routing successful.")
 
-        let expectedRoutingInstructions1: RoutingInstructions = [
-            .switchTo(
-                entry: RoutingEntry(identifier: .node2),
-                switchNodeEntry: RoutingEntry(identifier: .switchNode1)
-            ),
-            .enter(entry: RoutingEntry(identifier: .switchNode3)),
-            .switchTo(
-                entry: RoutingEntry(identifier: .node12),
-                switchNodeEntry: RoutingEntry(identifier: .switchNode3)
-            )
-        ]
-
-        let expectedRoutingInstructions2: RoutingInstructions = [
-            .leave(entry: RoutingEntry(identifier: .switchNode3)),
-            .switchTo(
-                entry: RoutingEntry(identifier: .node1),
-                switchNodeEntry: RoutingEntry(identifier: .switchNode1)
-            ),
-            .enter(entry: RoutingEntry(identifier: .node5)),
-            .enter(entry: RoutingEntry(identifier: .node8))
-        ]
-
-        let expectedRoutingInstructions3: RoutingInstructions = [
-            .leave(entry: RoutingEntry(identifier: .node8)),
-            .leave(entry: RoutingEntry(identifier: .node5)),
-            .switchTo(
-                entry: RoutingEntry(identifier: .switchNode2),
-                switchNodeEntry: RoutingEntry(identifier: .switchNode1)
-            ),
-            .switchTo(
-                entry: RoutingEntry(identifier: .node4),
-                switchNodeEntry: RoutingEntry(identifier: .switchNode2)
-            )
-        ]
-
-        Router.default.routeTo(
+        routeTo(
             staticDestination: RoutingTable.StaticEntries.staticDestination1,
-            animated: false
-        ) { result in
-            switch result {
-            case let .success(executedInstructions):
-                XCTAssertEqual(executedInstructions, expectedRoutingInstructions1)
-                exp1.fulfill()
-
-                Router.default.routeTo(
-                    staticDestination: RoutingTable.StaticEntries.staticDestination2,
-                    animated: false
-                ) { result in
-                    switch result {
-                    case let .success(executedInstructions):
-                        XCTAssertEqual(executedInstructions, expectedRoutingInstructions2)
-                        exp2.fulfill()
-
-                        Router.default.routeTo(
-                            staticDestination: RoutingTable.StaticEntries.staticDestination3,
-                            animated: false
-                        ) { result in
-                            switch result {
-                            case let .success(executedInstructions):
-                                XCTAssertEqual(executedInstructions, expectedRoutingInstructions3)
-                                exp3.fulfill()
-
-                            case .failure:
-                                XCTFail("Routing should have succeeded.")
-                            }
+            expectedRoutingInstructions: expectedRoutingInstructions1
+        ) { [unowned self] success in
+            XCTAssert(success)
+            self.routeTo(
+                staticDestination: RoutingTable.StaticEntries.staticDestination2,
+                expectedRoutingInstructions: self.expectedRoutingInstructions2
+            ) { [unowned self] success in
+                XCTAssert(success)
+                self.routeTo(
+                    staticDestination: RoutingTable.StaticEntries.staticDestination3,
+                    expectedRoutingInstructions: self.expectedRoutingInstructions3
+                ) { success in
+                    XCTAssert(success)
+                    self.routeTo(
+                        staticDestination: RoutingTable.StaticEntries.staticDestination4,
+                        expectedRoutingInstructions: self.expectedRoutingInstructions4
+                    ) { success in
+                        XCTAssert(success)
+                        self.routeTo(
+                            staticDestination: RoutingTable.StaticEntries.staticDestination5,
+                            expectedRoutingInstructions: self.expectedRoutingInstructions5
+                        ) { success in
+                            XCTAssert(success)
+                            exp.fulfill()
                         }
-
-                    case .failure:
-                        XCTFail("Routing should have succeeded.")
                     }
                 }
-
-            case .failure:
-                XCTFail("Routing should have succeeded.")
             }
         }
 
         waitForExpectations(timeout: 1, handler: nil)
+    }
+}
+
+// MARK: - Helper
+extension PortusTests {
+    private func routeTo(
+        staticDestination: StaticRoutingDestination,
+        expectedRoutingInstructions: RoutingInstructions,
+        completion: @escaping (Bool) -> Void
+        ) {
+        Router.default.routeTo(
+            staticDestination: staticDestination,
+            animated: false
+        ) { result in
+            switch result {
+            case let .success(executedInstructions):
+                XCTAssertEqual(executedInstructions, expectedRoutingInstructions)
+                completion(true)
+
+            case .failure:
+                completion(false)
+            }
+        }
     }
 }
